fe17abc3c2e5dc0fb4e3d0abcec234eeb8a2443c
Splatatraa <corentin.girault.audurier@gmail.com>
2021-11-27 18:04:09 +0100

    start of the project

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
+// importation modules
 const express = require('express')
+const fs = require('fs')
+const ejs = require('ejs')
 
+// apps express
+const site_client = express()
+const site_client_config = express()
 
-const app = express()
-
-const app_config = express()
-
+// constantes
 const port = 80
-
 const port_config = 8080
 
 
-pp.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// Début Site Client 
+
+site_client.use(express.urlencoded({ extended: true }))
+site_client.use(express.json())
 
 
-app.use('/', express.static(__dirname + '/site/'))
+site_client.use('/', express.static(__dirname + '/site/'))
 
-app.listen(port, () =>{
-    console.log(`Le serveur est bien lancé sur http://127.0.0.1:${port}`)
+site_client.listen(port, () =>{
+    console.log(`Le site client est bien lancé sur http://127.0.0.1:${port}`)
 })
 
 
-app.get ("/", (req, res)=>{
+site_client.get ("/", (req, res)=>{
         
 })
 
+//Fin du site client
+
+
+// Début site config_config
+
+site_client_config.use(express.urlencoded({ extended: true }))
+site_client_config.use(express.json())
+site_client_config.set('view engine', 'ejs')
+
+
+site_client_config.use('/', express.static(__dirname + '/site_config/'))
+
+site_client_config.listen(port_config, () =>{
+    console.log(`Le site config est bien lancé sur http://127.0.0.1:${port_config}`)
+})
+
+
+site_client_config.get ("/", async (req, res)=>{
+   
+    let list_fichier = []
+    
+    await fs.readdir("./site/", (err, files) => {
+        files.forEach(file => {
+          list_fichier.push(file)
+        })
+      })
+      console.log(list_fichier)
+
+    res.render(__dirname + "/site_config/index.ejs", {
+        'port' : port,
+        'liste' : list_fichier
+    })
+
+
+})
